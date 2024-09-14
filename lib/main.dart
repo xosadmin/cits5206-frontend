bcdf980178ebf6d164ff5e343ed489fdf85941bd
Nishanth Arul <nishantharul1997@gmail.com>
2024-09-14 11:51:12 +0530

    Added Dark Mode functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:audiopin_frontend/core/themes/theme.dart';
 import 'package:audiopin_frontend/pages/discover.dart';
 import 'package:flutter/material.dart';
-import 'pages/get_started.dart';
 import 'pages/welcome.dart';
 import 'pages/sign_in.dart';
 import 'pages/forgot_pwd.dart';
@@ -12,22 +12,34 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system; // Initially system theme
+
+  void _toggleThemeMode() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AudioPin',
-      theme: ThemeData(
-        // define global theme, such as color, font style
-        scaffoldBackgroundColor: const Color(0xFFFFFFFF),
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
-      home: const MusicPlayerPage(),
+      themeMode: _themeMode,
+      theme: ThemeClass.lightTheme,
+      darkTheme: ThemeClass.darkTheme,
+      home: MusicPlayerPage(toggleTheme: _toggleThemeMode),
       routes: {
         '/welcome': (context) => const Welcome(),
         '/sign_in': (context) => const SignInPage(),
@@ -35,7 +47,8 @@ class MyApp extends StatelessWidget {
         '/verify_mail': (context) => const VerifyEmailPage(),
         '/homepage': (context) => const HomePage(),
         '/discover': (context) => const DiscoverPage(),
-        '/music_player': (context) => const MusicPlayerPage()
+        '/music_player': (context) =>
+            MusicPlayerPage(toggleTheme: _toggleThemeMode),
       },
     );
   }
--- lib/pages/music_player.dart
+++ lib/pages/music_player.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class MusicPlayerPage extends StatefulWidget {
-  const MusicPlayerPage({super.key});
+  final VoidCallback toggleTheme; // Define the callback function
+
+  const MusicPlayerPage({super.key, required this.toggleTheme}); // Accept the callback in the constructor
   @override
   State<MusicPlayerPage> createState() => _MusicPlayerPageState();
 }
 
 class _MusicPlayerPageState extends State<MusicPlayerPage> {
- // bool _isClicked = true;
+  late bool _isDarkMode;
   @override
   Widget build(BuildContext context) {
+    _isDarkMode = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Now Playing'),
+        centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.keyboard_arrow_down_rounded),
           onPressed: () {
@@ -21,8 +25,16 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
         ),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.nightlight),
-            onPressed: (){},
+            icon: Icon(
+              _isDarkMode ? Icons.wb_sunny : Icons.nightlight, // Toggle icon based on theme
+            ),
+            onPressed: () {
+              widget.toggleTheme(); // Call the toggle function
+              setState(() {
+                // Update _isDarkMode after the theme changes
+                _isDarkMode = !_isDarkMode;
+              });
+            },
           ),
           IconButton(
             icon: const Icon(Icons.more_horiz),
@@ -32,5 +44,7 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
       ),
     );
   }
+
+
 }
 
